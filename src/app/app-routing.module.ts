ed73547c2b7d8672511b1381c0990a476d01f22d
unknown <nicolas.f.vasconcelos@gmail.com>
2021-03-21 16:25:14 -0300

    Roteirização de páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+//array de objetos de rotas 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //path vazio pra quando não tiver rota, ele redirecionar (redirectTo) pra tela que eu quero
+  //pathMatch = pra trazer a tela completa
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
+   //path=nome da rota --> falo que essa rota vai para um lugar (component: nomeComponent) 
+  {path:'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
